5d997f521de897be736afcf8101be483465522cc
chongkal <hommequirit.seng@gmail.com>
2014-06-18 22:55:16 +0400

    added a test class

---
+++ MBFTest.h
@@ -0,0 +1,13 @@
+//
+//  MBFTest.h
+//  GitTest
+//
+//  Created by CHONGKAL SENG on 18.06.14.
+//  Copyright (c) 2014 CHONGKAL SENG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBFTest : NSObject
+
+@end
---
+++ MBFTest.m
@@ -0,0 +1,13 @@
+//
+//  MBFTest.m
+//  GitTest
+//
+//  Created by CHONGKAL SENG on 18.06.14.
+//  Copyright (c) 2014 CHONGKAL SENG. All rights reserved.
+//
+
+#import "MBFTest.h"
+
+@implementation MBFTest
+
+@end
