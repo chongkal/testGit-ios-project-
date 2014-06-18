ba38b425ec0535f715f3d45ba681887d57840d1a
chongkal <hommequirit.seng@gmail.com>
2014-06-18 22:47:02 +0400

    Initial Commit

---
+++ GitTest/MBFViewController.h
@@ -0,0 +1,13 @@
+//
+//  MBFViewController.h
+//  GitTest
+//
+//  Created by CHONGKAL SENG on 18.06.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBFViewController : UIViewController
+
+@end
---
+++ GitTest/MBFViewController.m
@@ -0,0 +1,29 @@
+//
+//  MBFViewController.m
+//  GitTest
+//
+//  Created by CHONGKAL SENG on 18.06.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "MBFViewController.h"
+
+@interface MBFViewController ()
+
+@end
+
+@implementation MBFViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
